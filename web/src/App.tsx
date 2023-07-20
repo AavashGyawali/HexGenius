abe88f99ed38fcaaf79a1d2f2f9785ffc8992b66
Lion786420 <thapaaniket12345@gmail.com>
2023-07-20 10:30:09 +0545

    Search bar created

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 import Map from "./components/Map";
+import Searchbar from "./Searchbar";
 import "./App.css";
 
 function App() {
   const [address, setAddress] = useState("");
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <Map setAddress={setAddress} />
+    
+    <div>
+      <div style={{
+        position:'absolute',
+        top:'10px',
+        right:'10px',
+        zIndex:"500"
+      }}  >
+        <Searchbar/>
+      </div>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          position:"relative"
+        }}
+      >
+        <Map setAddress={setAddress} />
+      
+      </div>
     </div>
   );
 }
--- web/src/components/MapLocationSeter.tsx
+++ web/src/components/MapLocationSeter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/MapLocationSeter.styles.css"
 
 const MapLocationSeter = () => {
   return <div>MapLocationSeter</div>;
