ddbfecdeff0879f97371f7d6e64a5bf88325706d
Lion786420 <thapaaniket12345@gmail.com>
2023-07-20 14:23:47 +0545

    Static route structure created

--- web/src/RouteDetail.tsx
+++ web/src/RouteDetail.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Card from './Card'
+import Next from './Next'
 
 function RouteDetail() {
   return (
     <div className='route'>
         <div className='number'>Route</div>
+        <Card/>
+        <Next/>
+        <Card/>
+        <Next/>
         <Card/>
     </div>
   )
